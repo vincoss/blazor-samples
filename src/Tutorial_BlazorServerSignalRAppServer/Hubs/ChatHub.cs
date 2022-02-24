b8701c84c3974be8b27cb8a85ca9cb87c0c9b972
Ferdinand <feroalien@hotmail.com>
2022-02-24 11:35:13 +1000

    totorial SignalR

---
+++ src/Tutorial_BlazorServerSignalRAppServer/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Tutorial_BlazorServerSignalRAppServer.Hubs;
+
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
