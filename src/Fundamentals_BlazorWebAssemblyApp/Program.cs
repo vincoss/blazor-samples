ee10a110b9d23b0fd9f36339a9ff9242f6de0cc2
Ferdinand Lukasak <ferdinand@gadsolutions.com.au>
2022-03-08 16:20:24 +1000

    config samples

---
+++ src/Fundamentals_BlazorWebAssemblyApp/Program.cs
@@ -0,0 +1,41 @@
+using Fundamentals_BlazorWebAssemblyApp;
+using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Read config file into configuration JSON file
+var http = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+
+builder.Services.AddScoped(sp => http);
+
+using var response = await http.GetAsync("cars.json");
+using var stream = await response.Content.ReadAsStreamAsync();
+builder.Configuration.AddJsonStream(stream);
+
+// Memory Configuration Source
+var vehicleData = new Dictionary<string, string>()
+{
+    { "color", "blue" },
+    { "type", "car" },
+    { "wheels:count", "3" },
+    { "wheels:brand", "Blazin" },
+    { "wheels:brand:type", "rally" },
+    { "wheels:year", "2008" },
+};
+
+var memoryConfig = new MemoryConfigurationSource { InitialData = vehicleData };
+builder.Configuration.Add(memoryConfig);
+
+//builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+await builder.Build().RunAsync();
---
+++ src/Fundamentals_BlazorWebAssemblyApp/Readme.md
@@ -0,0 +1,3 @@
+﻿
+## Resources
+https://docs.microsoft.com/en-us/aspnet/core/blazor/fundamentals/?view=aspnetcore-6.0
