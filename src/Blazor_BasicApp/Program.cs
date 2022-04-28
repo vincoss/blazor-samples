4561a517c895f4bdf8db60fd19e6703be17ad51d
Ferdinand Lukasak <ferdinand@gadsolutions.com.au>
2022-04-28 14:12:23 +1000

    project

---
+++ src/Blazor_BasicApp/Program.cs
@@ -0,0 +1,11 @@
+using Blazor_BasicApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#appblazor");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
