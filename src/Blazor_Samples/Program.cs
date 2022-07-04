dfe36150a7719ad5cc42841e5bbadfd7d12bfc5c
Ferdinand <feroalien@hotmail.com>
2022-07-05 05:21:43 +1000

    working with data API

--- src/Blazor_Samples/Program.cs
+++ src/Blazor_Samples/Program.cs
@@ -8,4 +8,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Working with data
+builder.Services.AddHttpClient("github", client =>
+{
+    client.BaseAddress = new Uri("http://api.github.com/");
+    // Github API versioning
+    client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+    // Github requires a user-agent
+    client.DefaultRequestHeaders.Add("User-Agent", "Blazor_Samples");
+});
+
 await builder.Build().RunAsync();
