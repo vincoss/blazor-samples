f656b6a2a5a08745b8f17cc2e204ae48aa4729c3
fero <feroalien@hotmail.com>
2023-04-06 15:18:38 +1000

    default project upgrade

--- src/Default_BlazorWebAssemblyApp_ServerHosted_PWA/Client/Program.cs
+++ src/Default_BlazorWebAssemblyApp_ServerHosted_PWA/Client/Program.cs
@@ -2,10 +2,19 @@ using Default_BlazorWebAssemblyApp_ServerHosted_PWA.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace Default_BlazorWebAssemblyApp_ServerHosted_PWA.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
+    }
+}
