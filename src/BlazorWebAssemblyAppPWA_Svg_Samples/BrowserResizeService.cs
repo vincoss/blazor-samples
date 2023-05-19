d1e4afd4bcefca70a06ada8ec2ee2b40c6ef9abb
fero <feroalien@hotmail.com>
2023-05-19 14:10:12 +1000

    resize sample

---
+++ src/BlazorWebAssemblyAppPWA_Svg_Samples/BrowserResizeService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyAppPWA_Svg_Samples
+{
+    public class BrowserResizeService : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public BrowserResizeService(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./browser-resize.js").AsTask());
+        }
+
+        public async Task Initialize()
+        {
+            StaticOnLogAsync = this.OnResize;
+            var module = await moduleTask.Value;
+            await module.InvokeVoidAsync("registerResizeCallback");
+        }
+
+        private static Func<Task> StaticOnLogAsync;
+
+        public event Func<Task> OnResize = () => {  return Task.CompletedTask; };
+
+        [JSInvokable("OnBrowserResizeHandler")]
+        public static async Task OnBrowserResizeee()
+        {
+            await StaticOnLogAsync?.Invoke();           
+        }
+
+        public async Task<int> GetInnerHeight()
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<int>("getInnerHeight");
+        }
+
+        public async Task<int> GetInnerWidth()
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<int>("getInnerWidth");
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ src/BlazorWebAssemblyAppPWA_Svg_Samples/wwwroot/browser-resize.js
@@ -0,0 +1,15 @@
+﻿export function getInnerHeight() {
+    return window.innerHeight;
+};
+
+export function getInnerWidth() {
+    return window.innerWidth;
+};
+
+export function registerResizeCallback() {
+    window.addEventListener("resize", resized);
+};
+
+export function resized() {
+    DotNet.invokeMethodAsync("BlazorWebAssemblyAppPWA_Svg_Samples", 'OnBrowserResizeHandler').then(data => data);
+};
