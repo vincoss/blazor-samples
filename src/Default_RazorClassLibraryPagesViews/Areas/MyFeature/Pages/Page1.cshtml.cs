fdfc74b3f20feb5a9194f3144c2aeba37c5269c3
fero <feroalien@hotmail.com>
2023-11-29 10:30:52 +1000

    project added

---
+++ src/Default_RazorClassLibraryPagesViews/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Default_RazorClassLibraryPagesViews.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
