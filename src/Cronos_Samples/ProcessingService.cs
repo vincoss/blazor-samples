9c5993c7431bad955d74670fe0e57395ed43c4e8
fero <feroalien@hotmail.com>
2025-01-29 12:49:30 +1000

    cron sample

--- src/Cronos_Samples/ProcessingService.cs
+++ src/Cronos_Samples/ProcessingService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cronos;
+using System;
 
 
 namespace Cronos_Samples
@@ -14,7 +15,56 @@ namespace Cronos_Samples
         {
             Console.WriteLine($"{nameof(ProcessingService)}: {DateTime.Now}");
 
+            /*
+                Read here con expression from some source, config or database... 
+            */
+
+            var cronExpression = "*/1 * * * *";
+            Cro(cronExpression);
+
             return Task.CompletedTask;
+        }
+
+        private DateTime _lastRun = DateTime.MinValue;
+
+        private void Cro(string targetExpression)
+        {
+            var utcNow = DateTime.UtcNow;
+            var flag = CronExpression.TryParse(targetExpression, out CronExpression actualExpression);
+
+            if (actualExpression != null)
+            {
+                var nextOccurrence = actualExpression.GetNextOccurrence(utcNow);
+
+                if (nextOccurrence != null)
+                {
+                    Console.WriteLine($"Scheduled expression '{targetExpression}', with next occurrence is '{nextOccurrence}', utc now: '{utcNow}'.");
+
+                    if (IsSameMinute(_lastRun, nextOccurrence.Value))
+                    {
+                        Console.WriteLine("The scheduled expression this minute.");
+                        return;
+                    }
+
+                    if (IsSameMinute(utcNow, nextOccurrence.Value))
+                    {
+                        Console.WriteLine($"Running scheduled expression: '{nextOccurrence}' at utc now: '{utcNow}'.");
+
+                        // Run processing schedule
+
+                        _lastRun = utcNow;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Invalid cron expression: {targetExpression}");
+            }
+        }
+
+        private static bool IsSameMinute(DateTime dateTime1, DateTime dateTime2)
+        {
+            return (Math.Abs((dateTime1 - dateTime2).TotalMinutes) < 1);
         }
     }
 }
